a346553b870a65a43dd2fdbf32a3cebc69fa6666
ClarissaDarla <85683142+ClarissaDarla@users.noreply.github.com>
2021-06-10 18:29:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,23 @@
+var a=prompt("enter first number")
+var b=prompt("enter second number")
+
+
+function setup() {
+  var button = createButton("click here to swap")
+  button.mousePressed(swap)
+}
+
+function draw() {
+  
+}
+
+function swap() {
+[a,b]=[b,a]
+console.log("a="+a)
+console.log("b="+b)
+}
+
+
+
+
+
